bbcd6c650fe92b83b1673f466dd7a9c45b3ebb45
clemmbn <clem.mbn@gmail.com>
2025-06-25 23:30:05 -0300

    Initial Commit

---
+++ Enigmate/EnigmateApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnigmateApp.swift
+//  Enigmate
+//
+//  Created by Clément Maubon on 25/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct EnigmateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
